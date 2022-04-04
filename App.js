26c9ee524d147010be2405dc821849c838cbbae1
hochieng@webtribe.co.ke <hochieng@webtribe.co.ke>
2022-04-04 16:03:27 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+class Passwords extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+        <View style={styles.container}>
+          <Text>
+            Your Passwords
+          </Text>
+        </View>
+    )
+  }
+}
+
+class AddPassword extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+        <View style={styles.container}>
+          <Text>
+            New Password
+          </Text>
+        </View>
+    )
+  }
+}
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+                name={"Passwords"}
+                component={Passwords}
+                options={{title:"My passwords"}}
+                />
+
+            <Stack.Screen
+                name={"Add Password"}
+                component={AddPassword}
+                options={{title:"Add password"}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
